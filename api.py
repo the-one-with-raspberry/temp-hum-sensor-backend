ceb969c4c00c76d16ab3fbb2e883f189b2ab1082
cipriancostin <ciprian.costin@gmail.com>
2022-12-28 12:05:10 +0200

    of CORS i had to fix CORS

--- api.py
+++ api.py
@@ -11,6 +11,10 @@ def apidocs():
 @app.route('/api/v1/current/getInfo', methods=['GET'])
 def api_getInfo():
     with SHT21(1) as sht21:
-        return jsonify({'status': 200, 'content': {'humidity': {'value': round(sht21.read_humidity(), 1)}, 'farenheit': {'value': round(sht21.read_temperature(), 1) * (9/5) + 32, 'unit': 'farenheit'}, 'celsius': {'value': round(sht21.read_temperature(), 1), 'unit': 'celsius'}}}), 200
+        response = jsonify({'status': 200, 'content': {'humidity': {'value': round(sht21.read_humidity(), 1)}, 'farenheit': {'value': round(sht21.read_temperature(), 1) * (9/5) + 32, 'unit': 'farenheit'}, 'celsius': {'value': round(sht21.read_temperature(), 1), 'unit': 'celsius'}}})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response, 200
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, True)

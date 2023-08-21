0bae64a52e781807f3fa7add305bfbffda2cc8a1
berriz44 <72183318+the-one-with-raspberry@users.noreply.github.com>
2023-08-21 16:41:02 +0300

    Update dbupdate.py

--- dbupdate.py
+++ dbupdate.py
@@ -2,7 +2,7 @@ import datetime, psycopg2, schedule, time
 from sht21 import SHT21
 
 def pushDB():
-    conn = psycopg2.connect(database="pimyhouse", user="pi", password="123456", host="192.168.68.70", port=5432)
+    conn = psycopg2.connect(database="pimyhouse", user="pi", password="123456", host="localhost", port=5432)
     c = conn.cursor()
 
     with SHT21(1) as s:
